0f272a1767279371c58e9bcdf22d60c922435c0b
kristi <k.fidani@etogruppe.com>
2025-07-11 08:34:27 +0000

    update dependecies

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,13 @@ use crate::movie::MovieService;
 use db::DB;
 use std::env;
 use tonic::transport::Server;
-use tonic_health::server::health_reporter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db = DB::init().await?;
-    let (mut health_reporter, health_service) = health_reporter();
-    health_reporter
-        .set_serving::<MovieServer<MovieService>>()
-        .await;
 
     Server::builder()
-        .accept_http1(true)
-        .add_service(health_service)
-        .add_service(tonic_web::enable(MovieServer::new(MovieService::new(db))))
+        .add_service(MovieServer::new(MovieService::new(db)))
         .serve(format!("0.0.0.0:{}", env::var("PORT").unwrap_or("8080".to_string())).parse()?)
         .await?;
 

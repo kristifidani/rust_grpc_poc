4f86e08f4a8268b6ddcadbcdc8967dc89519303c
kristi <k.fidani@etogruppe.com>
2025-07-11 09:59:37 +0000

    make sure that the service is running and db connection

--- movie-service/bin/main.rs
+++ movie-service/bin/main.rs
@@ -1,21 +1,20 @@
-mod config;
-mod db;
-mod grpc;
-mod movie;
-
-use crate::grpc::movie::movie_server::MovieServer;
-use crate::movie::MovieService;
-use db::DB;
 use std::env;
+
+use movie_grpc_service::{db::DB, grpc::movie::movie_server::MovieServer, movie::MovieService};
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db = DB::init().await?;
 
+    let port = env::var("PORT").unwrap_or("8080".to_string());
+    let addr = format!("0.0.0.0:{}", port).parse()?;
+
+    println!("✅ Server running successfully on http://{}", addr);
+
     Server::builder()
         .add_service(MovieServer::new(MovieService::new(db)))
-        .serve(format!("0.0.0.0:{}", env::var("PORT").unwrap_or("8080".to_string())).parse()?)
+        .serve(addr)
         .await?;
 
     Ok(())
